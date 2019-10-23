3ce2a41b61a49e6d2cb812ad43a558cdd0b91908
Sebastian Cabezas <seba_audiplus@hotmail.com>
2019-10-23 03:00:09 -0300

    Arreglos varios parte 1

--- Proyecto/floreria/urls.py
+++ Proyecto/floreria/urls.py
@@ -1,8 +1,12 @@
 #urls de la pagina
 from django.contrib import admin
 from django.urls import path,include
-from .views import home
+from .views import home, galeria, quienes_somos,login,carrito
 
 urlpatterns = [
     path('',home,name='HOME'),
+    path('galeria',galeria,name='GALE'),
+    path('quienes_somos',quienes_somos,name='QUIEN'),
+    path('login',login,name='LOGIN'),
+    path('carrito',carrito,name='SHOP'),
 ]
--- Proyecto/floreria/views.py
+++ Proyecto/floreria/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'core/home.html')
+
+def galeria(request):
+    return render(request,'core/galeria.html')
+
+def login(request):
+    return render(request,'core/login.html')
+
+def quienes_somos(request):
+    return render(request,'core/quienes_somos.html')
+
+def carrito(request):
+    return render(request,'core/carrito.html')
